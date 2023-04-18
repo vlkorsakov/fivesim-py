0ff0d4c54ece9f84924f122524c8b935e7cc2fdc
vlkorsakov <vlkorsakov256@gmail.com>
2023-04-19 00:37:16 +0300

    added docs blank

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+project = "fivesim-py"
+copyright = "2023, vlkorsakov"
+author = "vlkorsakov"
+
+version = "0.1"
+release = "0.1"
+
+templates_path = ["_templates"]
+extensions = [
+]
+
+exclude_patterns = []
+source_suffix = ".rst"
+master_doc = "index"
+
+html_theme = "furo"
+html_static_path = ["_static"]
